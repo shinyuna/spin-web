631c63cea1a0418651dbdbaff3488c90de9578fb
minwoo <mwoo526@naver.com>
2019-02-12 00:45:45 +0900

    basic spin-web eventBus & Named View on 0212

---
+++ src/components/common/Footer.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="footer">
+    <p> footer</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Footer'
+}
+</script>
+
+<style scoped>
+  #footer{
+    position: absolute;
+    bottom: 0;
+  }
+</style>
---
+++ src/components/common/Header.vue
@@ -0,0 +1,53 @@
+<template>
+  <div id="header">
+    <p class="title">{{ message }}</p>
+  </div>
+</template>
+
+<script>
+import { eventBus } from '../../event'
+
+export default {
+  name: 'Header',
+  data () {
+    return {
+      message: ''
+    }
+  },
+  created () {
+    eventBus.$on('title', data => {
+      this.message = data
+    })
+  }
+}
+</script>
+
+<style scoped>
+  /* header */
+  #header{
+    width: 100%;
+    height: 70px;
+    background: transparent;
+    box-shadow: none;
+  }
+  #header .logo{
+    float: left;
+    width: 174px;
+    height: 70px;
+    padding-left: 20px;
+    object-fit: contain;
+  }
+  #header .logo a{
+    display: block;
+    width: 100%;
+    height: 100%;
+  }
+  .title{
+    float: right;
+    width: 70px;
+    margin-right: 30px;
+    line-height: 70px;
+    color: #eaedf8;
+    font-weight: 500;
+  }
+</style>
